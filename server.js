8fac66486741a4ab136a82823a0628038dcbeb10
Drew Bassett <73472116+drewbassett24@users.noreply.github.com>
2021-03-28 18:55:18 +0100

    Server working.

--- server.js
+++ server.js
@@ -1,16 +1,17 @@
 const express = require('express');
-const routes = require('./route');
-const sequelize = require('./config/connection');
-const Blogclear = require('./models');
+// const routes = require('./route');
+// const sequelize = require('./config/connection');
+// const Blogclear = require('./models');
+
+const PORT = 3307;
 
 const app = express();
-const PORT = process.env.PORT || 3306;
 
-app.use(routes);
+// app.use(routes);
 
-app.use(express.json());
-app.use(express.urlencoded({ extended: true }));
+// app.use(express.json());
+// app.use(express.urlencoded({ extended: true }));
 
-sequelize.sync().then(() => {
-  app.listen(PORT, () => console.log('Now listening'));
-});
+// sequelize.sync().then(() => {
+  app.listen(PORT, () => console.log('Now listening on PORT ' + PORT));
+// });
