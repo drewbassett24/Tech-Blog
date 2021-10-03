732be3014710a64537560e574c0ddaf8b3d4a9c7
Drew Bassett <73472116+drewbassett24@users.noreply.github.com>
2021-10-03 11:53:56 +0100

    corrected typo

--- controllers/api/userRoutes.js
+++ controllers/api/userRoutes.js
@@ -67,7 +67,7 @@ router.post('/login', async (req, res) => {
         }
     });
     
-    router.post('/logouot', (req, res) => {
+    router.post('/logout', (req, res) => {
         if (req.session.logged_in) {
             req.session.destroy(() => {
                 res.status(204).end;
