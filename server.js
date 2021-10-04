32679f1c0c5b93604a74633b7832d5b5c41ec0d8
Drew Bassett <73472116+drewbassett24@users.noreply.github.com>
2021-10-04 20:42:54 +0100

    Completed public functionality

--- server.js
+++ server.js
@@ -25,11 +25,13 @@ const sess = {
 
 app.use(session(sess));
 app.use(express.json());
+app.use(express.urlencoded({ extended: true}));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(routes);
+
 app.engine('handlebars', hbs.engine);
 app.set('view engine', 'handlebars');
-app.use(express.urlencoded({ extended: true}));
-app.use(routes);
-app.use(express.static(path.join(__dirname, 'public')));
 
 sequelize.sync({ force: false }).then(() => {
   app.listen(PORT, () => console.log('Now listening on PORT ' + PORT));
